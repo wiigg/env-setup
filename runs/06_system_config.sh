3f274357e35be2466ccf81c2f10557f380fef447
Danny Wigg <wiigg@users.noreply.github.com>
2025-03-03 20:33:55 +0000

    add missing system configs

--- runs/06_system_config.sh
+++ runs/06_system_config.sh
@@ -2,9 +2,16 @@
 set -euo pipefail
 set --
 
-defaults write com.apple.dock orientation -string "right"
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder AppleShowAllExtensions -bool true
+defaults write com.apple.finder ShowPathBar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
 
+defaults write com.apple.dock orientation -string "right"
 defaults write com.apple.dock tilesize -int 42
+
+defaults write com.apple.screencapture location "$HOME/Documents/Screenshots"
+defaults write com.apple.screencapture type -string "png"
 
 if ! command -v dockutil >/dev/null 2>&1; then
   echo "dockutil is not installed."
@@ -23,4 +30,6 @@ dockutil --add "/Applications/Spotify.app" --no-restart
 dockutil --add "/Applications/iTerm.app" --no-restart
 dockutil --add "/Applications/ChatGPT.app" --no-restart
 
+killall Finder
 killall Dock
+killall SystemUIServer
