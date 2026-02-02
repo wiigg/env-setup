1d0815f704562ef7404b853a89e5e8f1c7816b1a
Danny Wigg <wiigg@users.noreply.github.com>
2026-02-02 18:54:23 +0000

    fix(setup): wait for Xcode CLT install

--- runs/01_xcode.sh
+++ runs/01_xcode.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 if ! xcode-select -p &>/dev/null; then
     xcode-select --install
+    echo "Waiting for Xcode CLT to finish..."
+    until xcode-select -p &>/dev/null; do
+        sleep 10
+    done
 else
     echo "Xcode is already installed."
 fi
